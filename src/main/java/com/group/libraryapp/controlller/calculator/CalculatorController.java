eb27bedcd340cf4e2919adf6d401359178b7ed0c
이아름 <areum6321@gmail.com>
2024-04-29 17:18:37 +0900

    GET API 개발 - 덧셈 API 개발, @RequestParam 대신 Dto 사용

--- src/main/java/com/group/libraryapp/controlller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controlller/calculator/CalculatorController.java
@@ -1,14 +1,14 @@
 package com.group.libraryapp.controlller.calculator;
 
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //클래스를 api 진입지점으로 만들어준다.
 public class CalculatorController {
 
     @GetMapping("/add") //GET /add
-    public int addTwoNumbers(@RequestParam  int number1, @RequestParam int number2) {
-        return number1 + number2;
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNumber1() + request.getNumber2();
     }
 }
