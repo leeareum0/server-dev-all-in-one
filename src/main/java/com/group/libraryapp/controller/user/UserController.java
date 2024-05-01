5f59da38b8de6044b32db84525e62cb5c9eb7f3a
이아름 <areum6321@gmail.com>
2024-05-01 17:20:21 +0900

    Database 조작 - Spring에서 Database 사용, SQL 사용하기 위한 POST/GET API 변경

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,25 +1,34 @@
 package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.Fruit;
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    //필드 선언
+    private final JdbcTemplate jdbcTemplate;
+
+    //생성사 생성
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user") //POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)"; //입력값을 넣어야 해서 ? 사용
+        jdbcTemplate.update(sql, request.getName(), request.getAge()); //?값 순서대로 작성
     }
 
     @GetMapping("/fruit")
@@ -30,11 +39,18 @@ public class UserController {
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
 
-        List<UserResponse> responses = new ArrayList<>();
+        String sql = "SELECT * FROM user";
 
-        for (int i = 0; i < users.size(); i++) {
-            responses.add(new UserResponse(i + 1, users.get(i)));
-        }
-        return responses;
+        //익명 클래스
+        //RowMapper - sql 실행 후 user 정보를 선언한 타입인 UserResponse 전환해주는 역할
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+        });
     }
 }
