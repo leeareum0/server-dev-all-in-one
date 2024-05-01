08a2023d4aaaafd6a011a581de973dd5282b6654
이아름 <areum6321@gmail.com>
2024-05-01 16:42:54 +0900

    과제(2일차) - 문제3. 여러 수를 받아 총 합을 반환하는 POST API 개발, Dto 추가

---
+++ src/main/java/com/group/libraryapp/dto/ex/request/Ex3NumRequest.java
@@ -0,0 +1,18 @@
+package com.group.libraryapp.dto.ex.request;
+
+public class Ex3NumRequest {
+
+    private Integer[] numbers;
+
+    public Ex3NumRequest() {
+
+    }
+
+    public Ex3NumRequest(Integer[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public Integer[] getNumbers() {
+        return numbers;
+    }
+}
