1092cea25ac82e3cdec5de7e6e7ba93930610c57
이아름 <areum6321@gmail.com>
2024-05-03 15:35:08 +0900

    역할의 분리와 스프링 컨테이너 - Controller 기능 분리, 수정 및 삭제 Repository 분리

---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,34 @@
+package com.group.libraryapp.repository.user;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean isUserNotExist(long id) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
+
+    }
+
+    public void updateUserName(String name, long id) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+    public boolean isUserNotExist(String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+
+    }
+
+    public void deleteUser(String name) {
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+}
