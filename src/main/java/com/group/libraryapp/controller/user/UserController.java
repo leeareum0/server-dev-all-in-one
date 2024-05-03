f5f8d8ee8cde253169bae6e8a8b24bea915a2d6f
이아름 <areum6321@gmail.com>
2024-05-03 16:20:48 +0900

    역할의 분리와 스프링 컨테이너 - Controller 기능 분리, 조회 Controller 분리

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -14,13 +14,12 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-    private final JdbcTemplate jdbcTemplate;
 
     public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
         this.userService = new UserService(jdbcTemplate);
     }
 
+    //생성
     @PostMapping("/user") //POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
         userService.saveUser(request);
@@ -31,19 +30,11 @@ public class UserController {
         return new Fruit("바나나", 2000); //객체 반환
     }
 
+
+    //조회
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-
-        String sql = "SELECT * FROM user";
-
-        //익명 클래스
-        //RowMapper - sql 실행 후 user 정보를 선언한 타입인 UserResponse 전환해주는 역할(람다 사용)
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
     }
 
     //수정
