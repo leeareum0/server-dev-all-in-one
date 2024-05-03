d01e706792c1f27e8454adb16517c9f45c79d36a
이아름 <areum6321@gmail.com>
2024-05-03 15:34:18 +0900

    역할의 분리와 스프링 컨테이너 - Controller 기능 분리, 수정 및 삭제 기능 분리

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,6 +4,7 @@ import com.group.libraryapp.domain.user.Fruit;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +13,12 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    //필드 선언
+    private final UserService userService;
     private final JdbcTemplate jdbcTemplate;
 
-    //생성사 생성
     public UserController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.userService = new UserService(jdbcTemplate);
     }
 
     @PostMapping("/user") //POST /user
@@ -49,31 +50,12 @@ public class UserController {
     //수정
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        //유저 존재여부 확인
-        String readSql = "SELECT * FROM user WHERE id = ?";
-        //쿼리가 DB에 전송해서 데이터가 있는지 확인
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userService.updateUser(request);
     }
 
     //삭제
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-
-        String readSql = "SELECT * FROM user WHERE name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "DELETE FROM user WHERE name = ?";
-        jdbcTemplate.update(sql, name);
+        userService.deleteUser(name);
     }
 }
