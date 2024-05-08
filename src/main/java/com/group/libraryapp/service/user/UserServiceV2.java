7a43ab88f831a62c3f6d161a2fde1d179414f0cd
이아름 <areum6321@gmail.com>
2024-05-08 19:28:59 +0900

    Spring Data JPA 사용 - 유저 생성/조회/업데이트 기능 리팩토링하기, 유저 업데이트 기능 리팩토링 코드

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.service.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.domain.user.UserRepository;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,14 @@ public class UserServiceV2 {
     //조회
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream().map(UserResponse::new).collect(Collectors.toList());
+    }
+
+    //업데이트
+    public void updateUser(UserUpdateRequest request) {
+        User user = userRepository.findById(request.getId())
+                .orElseThrow(IllegalAccessError::new);  //유저가 없다면 예외처리
+
+        user.updateName(request.getName());
+        userRepository.save(user);
     }
 }
