2b029b192268b3ef3b86fd14cd914e8aef440008
이아름 <areum6321@gmail.com>
2024-05-08 19:49:43 +0900

    Spring Data JPA 사용 - 유저 삭제 기능 리팩토링 하기, 유저를 반환해주는 함수 추가

--- src/main/java/com/group/libraryapp/domain/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/domain/user/UserRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    //유저를 반환해주는 함수
+    User findByName(String name);
+
 }
