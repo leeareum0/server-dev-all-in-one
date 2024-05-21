53f604ba179b8a58cacf0e6e360bf961422157cb
이아름 <areum6321@gmail.com>
2024-05-21 17:40:32 +0900

    과제(7일차) - JPA 사용 및 코드 리팩토링, FruitService 구현

--- src/main/java/com/group/libraryapp/service/fruit/FruitService.java
+++ src/main/java/com/group/libraryapp/service/fruit/FruitService.java
@@ -1,11 +1,17 @@
 package com.group.libraryapp.service.fruit;
 
 
+import com.group.libraryapp.domain.user.Fruit;
 import com.group.libraryapp.dto.ex.request.Ex4FruitCreateRequest;
 import com.group.libraryapp.dto.ex.request.Ex4FruitSoldRequest;
 import com.group.libraryapp.dto.ex.response.Ex4FruitSalesAmountResponse;
+import com.group.libraryapp.dto.ex.response.FruitCountResponse;
+import com.group.libraryapp.dto.ex.response.FruitOptionResponse;
 import com.group.libraryapp.repository.fruit.FruitRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class FruitService {
@@ -17,20 +23,43 @@ public class FruitService {
     }
 
     public void saveFruit(Ex4FruitCreateRequest request) {
-        fruitRepository.saveFruit(request.getName(), request.getWarehousingDate(), request.getPrice());
+        fruitRepository.save(new Fruit(request.getName(), request.getWarehousingDate(), request.getPrice()));
     }
 
     public void updateFruit(Ex4FruitSoldRequest request) {
-        if (fruitRepository.isFruitNotExist(request.getId())) {
-            throw new IllegalArgumentException();
-        }
-        fruitRepository.updateFruit(request.getId());
+        Fruit fruit = fruitRepository.findById(request.getId()).orElseThrow(IllegalArgumentException::new);
+        fruit.updateIsSold(true);
+        fruitRepository.save(fruit);
     }
 
     public Ex4FruitSalesAmountResponse statFruit(String name) {
-        if (fruitRepository.isFruitNotExist(name)) {
+        long salesAmount = fruitRepository.findAllByNameAndIsSold(name,true)
+                .stream().mapToLong(Fruit::getPrice).sum();
+        long notSalesAmount = fruitRepository.findAllByNameAndIsSold(name,false)
+                .stream().mapToLong(Fruit::getPrice).sum();
+
+        return new Ex4FruitSalesAmountResponse(salesAmount, notSalesAmount);
+    }
+
+    public FruitCountResponse getFruitCount(String name) {
+        return new FruitCountResponse(fruitRepository.countAllByName(name));
+
+    }
+
+    public List<FruitOptionResponse> getFruitList(String option, long price) {
+        List<Fruit> fruits;
+
+        if (option.equals("GTE")) {
+            fruits = fruitRepository.findByPriceGreaterThanEqual(price);
+        } else if (option.equals("LTE")) {
+            fruits = fruitRepository.findByPriceLessThanEqual(price);
+        } else {
             throw new IllegalArgumentException();
         }
-        return fruitRepository.statFruit(name);
+
+        return fruits.stream()
+                .filter(fruit -> !fruit.isSold())
+                .map(FruitOptionResponse::new)
+                .toList();
     }
 }
