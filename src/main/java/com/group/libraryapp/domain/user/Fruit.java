60b1d99bc3c70ee67dac4c77ab818bc1c4c7c214
이아름 <areum6321@gmail.com>
2024-05-21 17:35:20 +0900

    과제(7일차) - JPA 사용 및 코드 리팩토링, Fruit Entity Class 사용

--- src/main/java/com/group/libraryapp/domain/user/Fruit.java
+++ src/main/java/com/group/libraryapp/domain/user/Fruit.java
@@ -1,20 +1,61 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Fruit {
 
-    private String name;
-    private int price;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Fruit(String name, int price) {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate warehousingDate;
+
+    private Long price;
+
+    @Column(nullable = false)
+    private Boolean isSold;
+
+    protected Fruit() {
+
+    }
+
+    public Fruit(String name, LocalDate warehousingDate, Long price) {
         this.name = name;
+        this.warehousingDate = warehousingDate;
         this.price = price;
+        this.isSold = false;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getPrice() {
+    public LocalDate getWarehousingDate() {
+        return warehousingDate;
+    }
+
+    public Long getPrice() {
         return price;
+    }
+
+    public Boolean getSold() {
+        return isSold;
+    }
+
+    public void updateIsSold(Boolean isSold) {
+        this.isSold = isSold;
+    }
+
+    public boolean isSold() {
+        return isSold;
     }
 }
