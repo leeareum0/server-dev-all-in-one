acd2434026160b51683645fcc2c90c425bc779ff
이아름 <areum6321@gmail.com>
2024-05-21 17:41:00 +0900

    과제(7일차) - JPA 사용 및 코드 리팩토링, FruitController 구현

--- src/main/java/com/group/libraryapp/controller/ex/fruit/FruitController.java
+++ src/main/java/com/group/libraryapp/controller/ex/fruit/FruitController.java
@@ -3,8 +3,12 @@ package com.group.libraryapp.controller.ex.fruit;
 import com.group.libraryapp.dto.ex.request.Ex4FruitCreateRequest;
 import com.group.libraryapp.dto.ex.request.Ex4FruitSoldRequest;
 import com.group.libraryapp.dto.ex.response.Ex4FruitSalesAmountResponse;
+import com.group.libraryapp.dto.ex.response.FruitCountResponse;
+import com.group.libraryapp.dto.ex.response.FruitOptionResponse;
 import com.group.libraryapp.service.fruit.FruitService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -29,5 +33,15 @@ public class FruitController {
     @GetMapping("/api/v1/fruit/stat")
     public Ex4FruitSalesAmountResponse statFruit(@RequestParam String name) {
         return fruitService.statFruit(name);
+    }
+
+    @GetMapping("/api/v1/fruit/count")
+    public FruitCountResponse getFruitCount(@RequestParam String name) {
+        return fruitService.getFruitCount(name);
+    }
+
+    @GetMapping("/api/v1/fruit/list")
+    public List<FruitOptionResponse> getFruitList(@RequestParam String option, @RequestParam long price) {
+        return fruitService.getFruitList(option, price);
     }
 }
